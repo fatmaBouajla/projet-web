0c2338b85cee5f26ccf5afa5bfae94a52c55d515
Fatma Bouajla <bouajlafattouma@gmail.com>
2025-05-10 00:08:26 +0200

    2

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-document.querySelector('.burger').addEventListener('click', function() {
-  document.querySelector('nav ul').classList.toggle('show');
-  this.classList.toggle('open');
-});
 
-// Header qui change au scroll
-window.addEventListener('scroll', function() {
-  document.querySelector('header').classList.toggle('scrolled', window.scrollY > 50);
-});
 
-// Compteur de panier
+
 let panier = 0;
 document.querySelectorAll('.add-to-cart').forEach(btn => {
   btn.addEventListener('click', function() {
